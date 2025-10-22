0557c3eebf99edebaed01371020f34071b969dc6
Anthony Tesija <anthony.tesija@discordapp.com>
2025-10-22 15:47:31 -0700

    More UI hooked up

--- DiscordSocialSDKUnitySample/Assets/Scripts/SettingsUI.cs
+++ DiscordSocialSDKUnitySample/Assets/Scripts/SettingsUI.cs
@@ -4,9 +4,54 @@ public class SettingsUI : MonoBehaviour
 {
     public GameObject friendsList;
     public GameObject settingsPanel;
+    public GameObject connectToDiscordPanel;
+    public GameObject accountLinkedPanel;
 
-    public void openSettings() {
+    public void openSettings()
+    {
         settingsPanel.SetActive(true);
         Debug.Log("Settings panel opened!");
+    }
+
+    public void CloseSettings()
+    {
+        settingsPanel.SetActive(false);
+    }
+
+    public void OpenDiscord()
+    {
+        string discordUrl = "https://discord.gg/discord-developers";
+        Application.OpenURL(discordUrl);
+    }
+
+    public void OpenConnectToDiscord()
+    {
+        settingsPanel.SetActive(false);
+        connectToDiscordPanel.SetActive(true);
+    }
+
+    public void CancelConnectToDiscord()
+    {
+        connectToDiscordPanel.SetActive(false);
+        settingsPanel.SetActive(true);
+    }
+
+    public void StartAuthFlow()
+    {
+
+    }
+
+    public void ShowAccountLinked()
+    {
+        connectToDiscordPanel.SetActive(false);
+        settingsPanel.SetActive(false);
+        accountLinkedPanel.SetActive(true);
+    }
+    
+    public void FinishAccountLinked()
+    {
+        connectToDiscordPanel.SetActive(false);
+        settingsPanel.SetActive(false);
+        accountLinkedPanel.SetActive(false);
     }
 }
--- DiscordSocialSDKUnitySample/Assets/Scripts/StartScreenUI.cs
+++ DiscordSocialSDKUnitySample/Assets/Scripts/StartScreenUI.cs
@@ -4,4 +4,9 @@ public class StartScreenUI : MonoBehaviour
 {
     public GameObject friendsList;
     public SettingsUI settingsUI;
+
+    public void OpenSettings()
+    {
+        settingsUI.openSettings();
+    }
 }
